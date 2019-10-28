4cbe971545d3af34087e14e7af9a90e8963271ed
cris <crist@LAPTOP-9D4R4P9I>
2019-10-28 12:13:27 +0000

    create fake repository

---
+++ SportsStore/Models/FakeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class FakeRepository : ISportStoreRepository
+    {
+        //criar lista falsa
+        public IEnumerable<Product> Products => new List<Product>
+        {
+            new Product{Name = "Football ball",Price = 25 },
+            new Product{Name = "Surf board",Price = 300 },
+            new Product{Name = "T-shirt",Price = 30 }
+            
+
+        };
+    }
+}
