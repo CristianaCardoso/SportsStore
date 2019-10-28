7ead527e6823118c74d660dd07b9adde9dfce6c3
cris <crist@LAPTOP-9D4R4P9I>
2019-10-28 12:12:46 +0000

    create Product

---
+++ SportsStore/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class Product
+    {
+        //chave primaria nome da tabela Product mais ID
+        public int ProductId { get; set; }
+
+
+        //nome obrigatorio
+        [Required]
+        public string Name { get; set; }
+
+        public string Descriptiom{get; set;}
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+    }
+}
