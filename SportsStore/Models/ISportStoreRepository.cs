03321099f35f962bc91ce9287622b931a2cf55ce
cris <crist@LAPTOP-9D4R4P9I>
2019-10-28 12:13:43 +0000

    create repository

---
+++ SportsStore/Models/ISportStoreRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public interface ISportStoreRepository
+    {
+        //percorrer todos os produtos
+        public IEnumerable<Product> Products { get;}
+    }
+}
